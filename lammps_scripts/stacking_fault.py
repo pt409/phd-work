b02d5d5c601fbec7b10fbae48c54495b507fa975
Pat <pt409@cam.ac.uk>
2019-12-11 17:52:20 +0000

    Updated .in script to produce output readable by python wrapper.

--- lammps_scripts/stacking_fault.py
+++ lammps_scripts/stacking_fault.py
@@ -21,14 +21,15 @@ start_time = time.time()
 
 ################################ PARAMETERS ###################################
 
-dx = -0.1 # Angstroms
+dx = -0.2 # Angstroms
 x = 0.0
+A = (3.52*np.sqrt(6)/2*10)*(3.52*np.sqrt(2)/2*10)
 
 Lammps.command(6,lammps_path="lammps")
 
-initial_run = Lammps.setup('stacking_fault_1.in')
+initial_run = Lammps.setup('stacking_fault_npt.in')
 
-initial_run = Lammps.update(initial_run,"run",update_dict={"variable x_displace":"equal 0.0"})
+initial_run = Lammps.update(initial_run,"npt_run",update_dict={"variable x_displace":"equal 0.0"})
 
 displacement = []
 E = []
@@ -37,10 +38,12 @@ while abs(x) < 3.52*np.sqrt(6)/2:
     current_run.run()
     displacement += [x]
     x += dx
-    with open(current_run.work_dir+"/"+current_run.log_file) as read_file:
-        f = read_file.readlines()
-        E += [float(f[-2].split()[-2])]
-        
+    #with open(current_run.work_dir+"/"+current_run.log_file) as read_file:
+    #    f = read_file.readlines()
+    #    E += [float(f[-2].split()[-2])]
+    output = current_run.read_log(["TotEng","Temp","Press"]) 
+    E += [np.mean(output[1][100000:])*1.60217657e4/A] # conversion factor from eV/A^2 to mJ/m^2
+    
 plt.plot(displacement,E,'bo-')
 plt.xlabel("Displacement (A)")
 plt.ylabel("SFE energy (mJ/m^2)")
