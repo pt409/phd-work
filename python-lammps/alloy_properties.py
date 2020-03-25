431a9605bc9a9f60744775cc18ea31c176e1b467
Pat <pt409@cam.ac.uk>
2020-03-25 10:51:44 +0000

    Fixed bugs in properties script.

--- python-lammps/alloy_properties.py
+++ python-lammps/alloy_properties.py
@@ -14,7 +14,7 @@ import numpy as np
 
 Lammps.command(20)
 
-N = 300
+N = 20
 
 elements = ["Cr","Co","Re","Ru","Al","Ta","W","Ti","Mo"]
 min_pc = [9.5,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0]
@@ -36,24 +36,26 @@ max_sum *= 0.01
                 [{"Ni":0.5248,"Cr":0.2558,"Co":0.1460,"Al":0.0139,"Ta":0.0080,"W":0.0254,"Mo":0.0262}]]"""
 
 with open("output","a+") as output:
-    output.write("\t".join(elements)+"\tsfe\tdensity\tlattice\tC11\tC12\tC44\n")
+    output.write("Ni\t"+"\t".join(elements)+"\tsfe\tdensity\tlattice\tC11\tC12\tC44\n")
 
 i = 0
 while i < N :
     composition = []
     pc_sum = 0
-    for i,el in enumerate(elements):
-        amount = (max_pc[i]-min_pc[i])*np.random.random()+min_pc[i]
+    for j,el in enumerate(elements):
+        amount = (max_pc[j]-min_pc[j])*np.random.random()+min_pc[j]
         composition += [amount]
         pc_sum += amount
     if pc_sum <= min_sum or pc_sum >= max_sum:
         pass
     else:
         composition = [1-pc_sum]+composition
-        with open("output","a+") as output:
-            output.write("\t".join(["%.4s" % x for x in composition])+"\t")
-        composition = dict(zip(elements,composition))
-        sfe,rho,a,c11,c12,c44 = alloy_md_properties(composition,"alloy_%d" %i,'sfe','density','lattice','C11','C12','C44')
-        with open("output","a+") as output:
-            output.write("%.5s\t%.5s\t%.6s\t%.5s\t%.5s\t%.5s\n" % (sfe,rho,a,c11,c12,c44))
-    i += 1
+        #with open("output","a+") as output:
+            #output.write("\t".join(["%.5s" % x for x in composition])+"\t")
+        print("\t".join(["%.5s" % x for x in composition])+"\t")
+        composition = dict(zip(["Ni"]+elements,composition))
+        print("alloy_%d" %i)
+        #sfe,rho,a,c11,c12,c44 = alloy_md_properties(composition,"alloy_%d" %i,'sfe','density','lattice','C11','C12','C44')
+        #with open("output","a+") as output:
+            #output.write("%.5s\t%.5s\t%.6s\t%.5s\t%.5s\t%.5s\n" % (sfe,rho,a,c11,c12,c44))
+        i += 1
