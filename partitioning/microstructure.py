cc07f1c41c90c05eceebc7639cf31105cd540323
Pat <pt409@cam.ac.uk>
2020-07-13 14:01:15 +0100

    Removed defunct code.

--- partitioning/microstructure.py
+++ partitioning/microstructure.py
@@ -37,37 +37,6 @@ def check_if_valid(input_,allow_all_null=False):
         else :
             return input_, 2 # Valid input
 
-# Get the valid data for a given header
-def grab_data(header,df=df,transform_ht=False,warn_if_insuff=False,dat_range=None):
-    # Provide header as a tuple
-    # transform_ht is a lambda function for x2 variables, temp and time
-    ht_num = df.loc[:,"Precipitation heat treatment"].shape[1]
-    el_num = df.loc[:,("Composition","at. %")].shape[1]
-    out_num = df.loc[:,[header]].shape[1] if len(header) >= 3 else df.loc[:,header].shape[1]
-    input_ = np.empty((0,ht_num//2+el_num-1),np.float64) if transform_ht else np.empty((0,ht_num+el_num-1),np.float64)
-    output = np.empty((0,out_num),np.float64)
-    for index, row in df.iterrows():
-        ht, ht_code = check_if_valid(row["Precipitation heat treatment"],allow_all_null=True)
-        comp, comp_code = check_if_valid(row["Composition","at. %"])
-        comp = comp[1:] # Remove value for Ni as it is not needed
-        entry, out_code = check_if_valid(row[header])
-        if ht_code==2 and comp_code==2 and out_code==2:
-            # If a further constraint has been applied that all of the data points should lie...
-            # ...within a certain range of values...
-            if dat_range:
-                if np.any(entry<dat_range[0]) or np.any(entry>dat_range[1]):
-                    continue
-            # Might want to apply a transformation to heat treatments: temp, time -> single value.
-            if transform_ht:
-                ht = np.array([transform_ht(ht_i[0],ht_i[1]) for ht_i in ht.reshape(ht_num//2,2)])
-            input_ = np.concatenate((input_,np.array([np.concatenate((ht,comp))])),axis=0)
-            output = np.concatenate((output,np.array([entry])),axis=0) if len(header)<3 else np.concatenate((output,np.array([[entry]])),axis=0)
-            
-    if warn_if_insuff: 
-        print("# data points = %d, input dims = %d" % (input_.shape[0],input_.shape[1]))
-        if input_.shape[0] <= input_.shape[1]: print("Insufficient # data points (%d) for fitting.\n" % input_.shape[0])
-    return np.array(input_), np.array(output)  
-
 # Get all the entries from the database with "complete" microstructure data.
 # "Complete" means complete composiiton + precipitate fraction + phase composition data.
 def get_microstructure_data(df,drop_duplicate_comps=False):
@@ -259,7 +228,7 @@ ml_data_dict = {}
 for el in elements:
     part_coeff_header = ("γ/γ’ partitioning ratio","at. %",el)
     if not part_coeff_header in ms_df:
-        part_coeff_header = ("γ/γ’ partitioning ratio","at. %",el+" ")
+        part_coeff_header = ("γ/γ’ partitioning ratio","at. %",el+" ") # In case there's a space after the element name in the database.
     ml_data_dict[el] = get_Xy(ms_df,part_coeff_header,min_max=[0.0,100.0],ht=incl_ht)
 ml_data_dict["f"] = get_Xy(ms_df,("γ’ fraction","at. %"),drop_na=False,flatten=True,ht=incl_ht)
 output_head = "        \t"+"       \t".join(elements + ["f"])
